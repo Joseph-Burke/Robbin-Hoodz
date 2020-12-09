9808f875d9ed54550a775b1b9371b6c502ad0ecc
Joseph Burke <joseph_burke@outlook.com>
2020-12-09 09:18:03 +0000

    Fix lint errors. Remove Stylelint from GH Actions

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-// import Phaser from 'phaser';
+import Phaser from 'phaser';
 
 export default {
   calculateNextPlatformHeight: (previousPlatform, player, game) => {
--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,18 +1,18 @@
-import Phaser from "phaser";
-import helpers from "./helpers";
+import Phaser from 'phaser';
+import helpers from './helpers';
 
 // calculateNextPlatformHeight;
 
 // getRandomSong;
 
-describe("gets a random song selection", () => {
-  let songInfo = helpers.getRandomSong();
-  let possibleTitles = ["bigPoppa", "putItOn", "californiaLove", "msJackson"];
-  let possiblePaths = [
-    "assets/music/big_poppa.mp3",
-    "assets/music/put_it_on.mp3",
-    "assets/music/california_love.mp3",
-    "assets/music/ms_jackson.mp3"
+describe('gets a random song selection', () => {
+  const songInfo = helpers.getRandomSong();
+  const possibleTitles = ['bigPoppa', 'putItOn', 'californiaLove', 'msJackson'];
+  const possiblePaths = [
+    'assets/music/big_poppa.mp3',
+    'assets/music/put_it_on.mp3',
+    'assets/music/california_love.mp3',
+    'assets/music/ms_jackson.mp3',
   ];
 
   it("returns an array where the first element is the key of one of the project's possible background tracks", () => {
@@ -27,44 +27,44 @@ describe("gets a random song selection", () => {
 // getPlatformImage;
 
 describe("gets the path of a platform image depending on a platform's length", () => {
-  let validKeys = ["plank1", "plank2", "plank3"];
-  let longPlatformLength = 201;
-  let shortPlatformLength = 199;
-  let randomPlatformLength = Phaser.Math.Between(100, 400);
+  const validKeys = ['plank1', 'plank2', 'plank3'];
+  const longPlatformLength = 201;
+  const shortPlatformLength = 199;
+  const randomPlatformLength = Phaser.Math.Between(100, 400);
 
-  it("always returns a valid image key", () => {
+  it('always returns a valid image key', () => {
     expect(
-      validKeys.includes(helpers.getPlatformImage(randomPlatformLength))
+      validKeys.includes(helpers.getPlatformImage(randomPlatformLength)),
     ).toBe(true);
   });
 
-  it("always gets the path of the longest platform image if the platform width is greater than 200px", () => {
-    expect(helpers.getPlatformImage(longPlatformLength) === "plank3").toBe(
-      true
+  it('always gets the path of the longest platform image if the platform width is greater than 200px', () => {
+    expect(helpers.getPlatformImage(longPlatformLength) === 'plank3').toBe(
+      true,
     );
   });
 
-  it("always gets the path of one of the shorter platform images if the platform width is less than 200px", () => {
+  it('always gets the path of one of the shorter platform images if the platform width is less than 200px', () => {
     expect(
-      ["plank1", "plank2"].includes(
-        helpers.getPlatformImage(shortPlatformLength)
-      )
+      ['plank1', 'plank2'].includes(
+        helpers.getPlatformImage(shortPlatformLength),
+      ),
     ).toBe(true);
   });
 });
 
 describe("saves an input's value to localStorage", () => {
-  let input = document.createElement("input");
+  const input = document.createElement('input');
 
   it("saves the given input's value attribute as the value for the localStorage 'username' key", () => {
-    input.value = "Test123";
+    input.value = 'Test123';
     helpers.submitNameForm(input);
-    expect(localStorage.getItem("username") === "Test123").toBe(true);
+    expect(localStorage.getItem('username') === 'Test123').toBe(true);
   });
 
   it("saves the string 'Anonymous' as the value for the localStorage 'username' key if input.value is empty", () => {
     input.value = '';
     helpers.submitNameForm(input);
-    expect(localStorage.getItem("username") === "Anonymous").toBe(true);
+    expect(localStorage.getItem('username') === 'Anonymous').toBe(true);
   });
 });
